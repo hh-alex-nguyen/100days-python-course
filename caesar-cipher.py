4ebc9eccee5660d0620d57cc5294ff76ef3952b5
hh-alex-nguyen <hai.nghoang00@gmail.com>
2024-03-31 23:02:09 +0700

    idea

---
+++ caesar-cipher.py
@@ -0,0 +1,17 @@
+character = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+             'v', 'w', 'x', 'y', 'z']
+
+
+# def caesarCipher(text, shift):
+#     text = text.lower()
+#     table = str.maketrans()
+#     text = text.translate(str.maketrans(character[shift:] + character[:shift]))
+#     return text
+#
+#
+# text = input('Password to encrypt')
+# shift = int(input('Enter shift'))
+# print(caesarCipher(text, shift))
+
+print(character[3:])
+print(character[:3])
