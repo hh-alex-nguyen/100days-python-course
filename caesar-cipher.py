8a2db4580452feaeb3b0df0dba4d1271f42ec234
hh-alex-nguyen <hai.nghoang00@gmail.com>
2024-04-02 09:18:40 +0700

    Test git laptop

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,7 +1,6 @@
 character = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
              'v', 'w', 'x', 'y', 'z']
 
-
 # def caesarCipher(text, shift):
 #     text = text.lower()
 #     table = str.maketrans()
@@ -15,3 +14,4 @@ character = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n
 
 print(character[3:])
 print(character[:3])
+# abc
