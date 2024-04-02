1e19d1f500e5f5dcf67e32f49cd67c3b8bc11ee7
hh-alex-nguyen <hai.nghoang00@gmail.com>
2024-04-02 10:04:45 +0700

    Caesar Cipher

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,17 +1,21 @@
 character = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
              'v', 'w', 'x', 'y', 'z']
 
-# def caesarCipher(text, shift):
-#     text = text.lower()
-#     table = str.maketrans()
-#     text = text.translate(str.maketrans(character[shift:] + character[:shift]))
-#     return text
-#
-#
-# text = input('Password to encrypt')
-# shift = int(input('Enter shift'))
-# print(caesarCipher(text, shift))
 
-print(character[3:])
-print(character[:3])
-# abc
+def caesar_cipher(text="abc", shift=1):
+    """
+    Caesar Cipher return the cipher text in alphabetical order with specified shift
+    """
+    text = text.lower()
+    shift = shift % len(character)
+    shift_char = character[shift:] + character[:shift]
+    # maketrans argument must be str not list
+    table = str.maketrans(''.join(character), ''.join(shift_char))
+    text = text.translate(table)
+    return text
+
+
+text = input('Password to encrypt: ')
+shift = int(input('Enter shift: '))
+print(caesar_cipher(text, shift))
+
